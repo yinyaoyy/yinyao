fbe206243b7dcf06028e24050916ee4fe0f53cb3
yinyao <715130775@qq.com>
2017-12-28 21:47:15 +0800

    111

--- src/main/java/com/yinyao/practice/practice.java
+++ src/main/java/com/yinyao/practice/practice.java
@@ -1,11 +1,24 @@
 package com.yinyao.practice;
 
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+
 public class practice {
 
 	public static void main(String[] args) {
 
-		int a = 1;
+		Queue<Integer> list = new ArrayBlockingQueue<Integer>(10);
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		
+		int a = list.peek();
 		System.out.println(a);
+		for(int t:list) {
+			System.out.println(t);
+		}
 	}
 
 }
